a3165dfc432407a9f2cbe151f002ed9942f79105
Mo Chen <sth4nth@gmail.com>
2013-11-04 17:18:38 +0800

    refactorize EM and FP algorithms for rvm classification

--- chapter07/rvmEbFp.m
+++ chapter07/rvmEbFp.m
@@ -3,12 +3,13 @@ function [model, llh] = rvmEbFp(X, t, alpha)
 % using fix point update (Mackay update)
 % Written by Mo Chen (sth4nth@gmail.com).
 if nargin < 3
-    alpha = 0.02;
+    alpha = 1;
 end
 n = size(X,2);
 X = [X;ones(1,n)];
 d = size(X,1);
 alpha = alpha*ones(d,1);
+weight = zeros(d,1);
 
 tol = 1e-4;
 maxiter = 100;
@@ -16,21 +17,20 @@ llh = -inf(1,maxiter);
 infinity = 1e+10;
 for iter = 2:maxiter
     used = alpha < infinity;
-    alphaUsed = alpha(used);
-    [w,V,lllh] = optLogitNewton(X(used,:),t,alphaUsed);  % lllh = logitloglikelihood
+    a = alpha(used);
+    w = weight(used); 
+    [w,energy,U] = optLogitNewton(X(used,:),t,a,w);  
     w2 = w.^2;
-    
-    logdetS = -2*sum(log(diag(V)));
-    llh(iter) = lllh+0.5*(sum(log(alphaUsed))-logdetS-dot(alphaUsed,w2)-n*log(2*pi)); % 7.114
+    llh(iter) = energy(end)+0.5*(sum(log(a))-2*sum(log(diag(U)))-dot(a,w2)-n*log(2*pi)); % 7.114
     if abs(llh(iter)-llh(iter-1)) < tol*llh(iter-1); break; end
-
-    dgSigma = dot(V,V,2);
-    gamma = 1-alphaUsed.*dgSigma;   % 7.89
-    alpha(used) = gamma./w2;           % 7.87
+    V = inv(U);
+    dgS = dot(V,V,2);
+    alpha(used) = (1-a.*dgS)./w2;       % 7.89 & 7.87
 end
 llh = llh(2:iter);
 
 model.used = used;
-model.w = w;
+model.w = w;                   % nonzero elements of weight
+model.a = a;                   % nonzero elements of alpha
+model.weght = weight;
 model.alpha = alpha;
-
