4856f169360f024dc85bab6aba8de66b96d0328c
Mo Chen <sth4nth@gmail.com>
2013-11-06 05:26:59 +0800

    demo change

--- chapter14/demo.m
+++ chapter14/demo.m
@@ -1,20 +1,25 @@
 clear all; close all
 %% Test for mixLinReg
-d = 1;
-k = 3;
-n = 500;
-W = randn(d+1,k);
+% d = 1;
+% k = 3;
+% n = 500;
+% W = randn(d+1,k);
+% 
+% [x, label] = rndKmeans(d, k, n);
+% X = [x; ones(1,n)];
+% y = zeros(1,n);
+% for j = 1:k
+%     idx = (label == j);
+%     y(idx) = W(:,j)'*X(:,idx);
+% end
+% 
+% plot(x,y,'.');
+% [model, label,llh] = mixLinReg(X, y, 3);
+% spread([x;y],label);
+% figure
+% plot(llh);
 
-[x, label] = rndKmeans(d, k, n);
-X = [x; ones(1,n)];
-y = zeros(1,n);
-for j = 1:k
-    idx = (label == j);
-    y(idx) = W(:,j)'*X(:,idx);
-end
-
-plot(x,y,'.');
-[model, label,llh] = mixLinReg(X, y, 3);
-spread([x;y],label);
-figure
-plot(llh);
+%%
+[X, y] = rndKmeans(2,3,1000);
+[label,L] = mixGaussVb(X, 10);
+plot(L);
