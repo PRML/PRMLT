8e03be875ca5364eb3b10057d589028706b850d7
Mo Chen <sth4nth@gmail.com>
2015-12-03 15:51:02 +0800

    add plotting and data generating functions

---
+++ chapter03/linPlot.m
@@ -0,0 +1,7 @@
+function linPlot(model, range)
+n = 100;
+x = linspace(range(1),range(2),n);
+y = lin;
+plot(x,y,'r-');
+end
+
---
+++ chapter03/linPredict.m
@@ -0,0 +1,20 @@
+function [y, sigma, p] = linPredict(model, X, t)
+% Compute linear model reponse y = w'*X+w0 and likelihood
+% model: trained model structure
+% X: d x n testing data
+% t (optional): 1 x n testing response
+% Written by Mo Chen (sth4nth@gmail.com).
+w = model.w;
+w0 = model.w0;
+y = w'*X+w0;
+if nargout == 3
+    beta = model.beta;
+    if isfield(model,'V')   % V*V'=inv(S) 3.54
+        U = model.V'*bsxfun(@minus,X,model.xbar);
+        sigma = sqrt(1/beta+dot(U,U,1));   % 3.59
+    else
+        sigma = sqrt(1/beta);
+    end
+    p = exp(logGauss(t,y,sigma));
+%     p2 = exp(-0.5*(((t-y)./sigma).^2+log(2*pi))-log(sigma));
+end
