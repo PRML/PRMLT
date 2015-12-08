00f5827d3d472ee938ed967c641fd96d021c4ab8
Mo Chen <sth4nth@gmail.com>
2015-12-08 17:40:25 +0800

    remove empty cluster check due to the fk unique bug

--- chapter09/kmeans.m
+++ chapter09/kmeans.m
@@ -7,11 +7,8 @@ n = size(X,2);
 last = 0;
 label = ceil(k*rand(1,n));  % random initialization
 while any(label ~= last)
-    [u,~,label] = unique(label);   % remove empty clusters
-    k = length(u);
     E = sparse(1:n,label,1,n,k,n);  % transform label into indicator matrix
     m = X*(E*spdiags(1./sum(E,1)',0,k,k));    % compute m of each cluster
     last = label;
     [~,label] = max(bsxfun(@minus,m'*X,dot(m,m,1)'/2),[],1); % assign samples to the nearest centers
 end
-[~,~,label] = unique(label);
