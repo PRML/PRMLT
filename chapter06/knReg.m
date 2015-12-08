6b6487f7494b5f47c9d05b8796d9cf1aa3af9b4c
Mo Chen <sth4nth@gmail.com>
2015-12-08 18:02:08 +0800

    add variance estimation in knReg.m

--- chapter06/knReg.m
+++ chapter06/knReg.m
@@ -1,5 +1,5 @@
 function model = knReg(X, t, lambda, kn)
-% Gaussian process for regression
+% Gaussian process (kernel) regression
 % Written by Mo Chen (sth4nth@gmail.com).
 if nargin < 4
     kn = @knGauss;
@@ -9,10 +9,13 @@ if nargin < 3
 end
 K = knCenterize(kn,X);
 tbar = mean(t);
-U = chol(K+lambda*eye(size(X,2)));
-a = U\(U'\(t(:)-tbar));  
+U = chol(K+lambda*eye(size(X,2)));    % 6.62
+a = U\(U'\(t(:)-tbar));               % 6.68
+y = a'*K+tbar;
+beta = 1/mean((t-y).^2);              % 3.21
 
 model.kn = kn;
 model.a = a;
 model.X = X;
 model.tbar = tbar;
+model.beta = beta;
