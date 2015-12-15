35ca9c1cd8cc4bc3beb09d2e3f64718e0f9209dd
Mo Chen <sth4nth@gmail.com>
2015-12-15 15:23:46 +0800

    init commit knPca.m

--- chapter06/knPca.m
+++ chapter06/knPca.m
@@ -1,6 +1,19 @@
-function [ output_args ] = knPca( input_args )
+function [ R, Z, err ] = knPca( X, d, kn )
 % Kernel PCA
-
-
+if nargin < 3
+    kn = @knGauss;
 end
 
+K = kn(X,X);
+K = knCenter(K);
+[V,A] = eig(K);
+[A,idx] = sort(diag(A),'descend');
+V = V(:,idx(1:d))';
+A = A(1:d);
+R = bsxfun(@times,V,1./sqrt(A));
+if nargout > 1    
+    Z = bsxfun(@times,V,sqrt(A));
+end
+if nargout > 2
+    err = diag(K)'-sum(Z.^2,1);
+end
