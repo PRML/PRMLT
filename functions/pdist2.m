ecdc289cce5825b22cac13b013ae7d15426e915f
Mo Chen <sth4nth@gmail.com>
2015-12-25 11:55:27 +0800

    remove depercate dirs

--- functions/pdist2.m
+++
@@ -1,6 +0,0 @@
-function D = pdist2(X1, X2)
-% Pairwise square Euclidean distance between two sample sets
-%   X1, X2: dxn1 dxn2 sample matrices
-% Written by Mo Chen (sth4nth@gmail.com).
-D = bsxfun(@plus,dot(X2,X2,1),dot(X1,X1,1)')-2*(X1'*X2);
-
--- helper/maxabsdiff.m
+++
@@ -1,4 +0,0 @@
-function z = maxabsdiff(x, y)
-% Written by Mo Chen (sth4nth@gmail.com).
-z = max(abs(x(:)-y(:)));
-
