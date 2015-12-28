b406341bef22bf28e21c48e5936090f7accd46d0
Mo Chen <sth4nth@gmail.com>
2015-12-28 10:18:33 +0800

    Bayesian regression need rewrite include sparse and linear

--- chapter07/rvmRegEbEm.m
+++ chapter07/rvmRegEbEm.m
@@ -6,48 +6,47 @@ if nargin < 3
     alpha = 0.02;
     beta = 0.5;
 end
-[d,n] = size(X);
+% xbar = mean(X,2);
+% tbar = mean(t,2);
+% X = bsxfun(@minus,X,xbar);
+% t = bsxfun(@minus,t,tbar);
+n = size(X,2);
+X = [X;ones(1,n)];
+d = size(X,1);
+
+% XX = X*X';
+% Xt = X*t';
+
 alpha = alpha*ones(d,1);
-
-xbar = mean(X,2);
-tbar = mean(t,2);
-
-X = bsxfun(@minus,X,xbar);
-t = bsxfun(@minus,t,tbar);
-
-XX = X*X';
-Xt = X*t';
-
 tol = 1e-4;
 maxiter = 500;
 llh = -inf(1,maxiter+1);
-infinity = 1e+8;
+infinity = 1e8;
+index = 1:d;
 for iter = 2 : maxiter
-    used = alpha < infinity;
-    d = sum(used);
-    alphaUsed = alpha(used);
-    S = beta*XX(used,used)+diag(alphaUsed);
-    U = chol(S);   
-    V = U\eye(d);    
-    w = beta*(V*(V'*Xt(used)));               % 7.82    
-    w2 = w.^2;
-    err = sum((t-w'*X(used,:)).^2);
+    nz = alpha < infinity;   % nonzeros
+    index = index(nz);
+    alpha = alpha(nz);
+    X = X(nz,:);
     
-    logdetS = -2*sum(log(diag(V)));    
-    llh(iter) = 0.5*(sum(log(alphaUsed))+n*log(beta)-beta*err-logdetS-dot(alphaUsed,w2)-n*log(2*pi)); 
-    if abs(llh(iter)-llh(iter-1)) < tol*llh(iter-1); break; end
+    S = inv(beta*(X*X')+diag(alpha));
+    % E-step
+    w = beta*S*X*t';   % E[w]     % 7.82
+    w2 = m.^2+diag(S);       % E[w^2]
+    e = sum((t-m'*X).^2);
 
-    dgS = dot(V,V,2);
-    alpha = 1./(w2+dgS);    % 9.67
+%     logdetS = -2*sum(log(diag(V)));    
+%     llh(iter) = 0.5*(sum(log(alpha))+n*log(beta)-beta*e-logdetS-dot(alpha,w2)-n*log(2*pi)); 
+%     if abs(llh(iter)-llh(iter-1)) < tol*llh(iter-1); break; end
 
-    gamma = 1-alphaUsed.*dgS;   % 7.89
-    beta = n/(err+sum(gamma)/beta);    % 9.68
+    % M-step
+    alpha = 1./w2;    % 9.67
+    beta = n/(e+sum(w2));    % 9.68 is wrong
 end
 llh = llh(2:iter);
 
-w0 = tbar-dot(w,xbar(used));
 
-model.used = used;
+model.index = index;
 model.w0 = w0;
 model.w = w;
 model.alpha = alpha;
