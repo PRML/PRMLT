5be1f4ec3bd243a7ea65e55427406af4bcfb53a3
chenmo <chenmo@sensenets.com>
2015-12-31 13:44:15 +0800

    delete never used log1pexp function

--- common/log1pexp.m
+++
@@ -1,7 +0,0 @@
-function y = log1pexp(x)
-% accurately compute y = log(1+exp(x))
-% reference: Accurately Computing log(1-exp(|a|)) Martin Machler
-seed = 33.3;
-y = x;
-idx = x<seed;
-y(idx) = log1p(exp(x(idx)));
