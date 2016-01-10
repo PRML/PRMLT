e90a1bdd2c8384e36dd86a9f012fa9c8473321d8
Mo Chen <sth4nth@gmail.com>
2016-01-10 13:47:25 +0800

    add comment

--- chapter07/rvmRegEbSeq.m
+++ chapter07/rvmRegEbSeq.m
@@ -4,24 +4,24 @@ function [model, llh] = rvmRegEbSeq(X, t)
 % reference:
 % Tipping and Faul. Fast marginal likelihood maximisation for sparse Bayesian models. AISTATS 2003.
 % Written by Mo Chen (sth4nth@gmail.com).
+maxiter = 1000;
+llh = -inf(1,maxiter);
+tol = 1e-6;
+
 [d,n] = size(X);
 xbar = mean(X,2);
 tbar = mean(t,2);
 X = bsxfun(@minus,X,xbar);
 t = bsxfun(@minus,t,tbar);
 
-beta = 0.01/(0.1*var(t))^2;   % beta = 1/sigma^2
+beta = 1/mean(t.^2);   % beta = 1/sigma^2
 alpha = inf(d,1);
-S = beta*dot(X,X,2);     % ?
-Q = beta*(X*t');         % ?
+S = beta*dot(X,X,2);     % eq.(22)
+Q = beta*(X*t');         % eq.(22)
 Sigma = zeros(0,0);  
 mu = zeros(0,1);  
 index = zeros(0,1);
 Phi = zeros(0,n);
-
-maxiter = 1000;
-tol = 1e-6;
-llh = -inf(1,maxiter);
 iAct = zeros(d,3);   
 for iter = 2:maxiter
     s = S; q = Q; % p.353 Execrcies 7.17
@@ -40,15 +40,15 @@ for iter = 2:maxiter
     
     dllh = -inf(d,1);  % delta likelihood
     if any(iUpd)
-        alpha_ = s(iUpd).^2./theta(iUpd);
+        alpha_ = s(iUpd).^2./theta(iUpd);      % eq.(20)
         delta = 1./alpha_-1./alpha(iUpd);
-        dllh(iUpd) = Q(iUpd).^2.*delta./(S(iUpd).*delta+1)-log1p(S(iUpd).*delta);
+        dllh(iUpd) = Q(iUpd).^2.*delta./(S(iUpd).*delta+1)-log1p(S(iUpd).*delta);  % eq.(32)
     end
     if any(iAdd)
-        dllh(iAdd) = (Q(iAdd).^2-S(iAdd))./S(iAdd)+log(S(iAdd)./(Q(iAdd).^2));
+        dllh(iAdd) = (Q(iAdd).^2-S(iAdd))./S(iAdd)+log(S(iAdd)./(Q(iAdd).^2));    % eq.(27)
     end
     if any(iDel)
-        dllh(iDel) = Q(iDel).^2./(S(iDel)-alpha(iDel))-log1p(-S(iDel)./alpha(iDel));
+        dllh(iDel) = Q(iDel).^2./(S(iDel)-alpha(iDel))-log1p(-S(iDel)./alpha(iDel));  % eq.(37)
     end
 
     [llh(iter),j] = max(dllh);
@@ -69,12 +69,12 @@ for iter = 2:maxiter
             mu_j = mu(idx);
             
             kappa = 1/(Sigma_jj+1/(alpha_-alpha(j)));
-            Sigma = Sigma-kappa*(Sigma_j*Sigma_j');                    % eq (33)
-            mu = mu-kappa*mu_j*Sigma_j;                                  % eq (34)
+            Sigma = Sigma-kappa*(Sigma_j*Sigma_j');                    % eq.(33)
+            mu = mu-kappa*mu_j*Sigma_j;                                  % eq.(34)
 
             v = beta*X*(Phi'*Sigma_j);
-            S = S+kappa*v.^2;                   % eq (35)
-            Q = Q+kappa*mu_j*v; 
+            S = S+kappa*v.^2;                   % eq.(35)
+            Q = Q+kappa*mu_j*v;                 % eq.(36)
             alpha(j) = alpha_;
         case 2 % Add
             alpha_ = s(j)^2/theta(j);
@@ -83,14 +83,14 @@ for iter = 2:maxiter
             phi_j = X(j,:);             
 
             v = beta*Sigma*(Phi*phi_j');  
-            off = -Sigma_jj*v;                         % ?
-            Sigma = [Sigma+Sigma_jj*(v*v'), off; off', Sigma_jj];
-            mu = [mu-mu_j*v; mu_j];
+            off = -Sigma_jj*v;                         % eq.(28) has error?
+            Sigma = [Sigma+Sigma_jj*(v*v'), off; off', Sigma_jj];   % eq.(28)
+            mu = [mu-mu_j*v; mu_j];                                % eq.(29)
             
             e_j = phi_j-v'*Phi;
             v = beta*X*e_j';
-            S = S-Sigma_jj*v.^2;
-            Q = Q-mu_j*v;
+            S = S-Sigma_jj*v.^2;                      % eq.(30)
+            Q = Q-mu_j*v;                              % eq.(31)
             
             index = [index;j]; %#ok<AGROW>
             alpha(j) = alpha_;
@@ -100,13 +100,12 @@ for iter = 2:maxiter
             Sigma_jj = Sigma(idx,idx);
             mu_j = mu(idx);
             
-            kappa = 1/Sigma_jj;
-            Sigma = Sigma-kappa*(Sigma_j*Sigma_j');                    % eq (33)
-            mu = mu-kappa*mu_j*Sigma_j;                                  % eq (34)
+            Sigma = Sigma-(Sigma_j*Sigma_j')/Sigma_jj;                    % eq.(38)
+            mu = mu-mu_j*Sigma_j/Sigma_jj;                                  % eq.(39)
 
             v = beta*X*(Phi'*Sigma_j);
-            S = S+kappa*v.^2;                   % eq (35)
-            Q = Q+kappa*mu_j*v; 
+            S = S+v.^2/Sigma_jj;                   % eq.(40)
+            Q = Q+mu_j*v/Sigma_jj;                  % eq.(41)
             
             mu(idx) = [];
             Sigma(:,idx) = [];
@@ -115,7 +114,7 @@ for iter = 2:maxiter
             alpha(j) = inf;
     end
     Phi = X(index,:); 
-%     beta = (n-d+dot(alpha(index),diag(Sigma)))/sum((t-mu'*Phi).^2);
+%     beta = ;
 end
 llh = llh(2:iter);
 w0 = tbar-dot(mu,xbar(index));
