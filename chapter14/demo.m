5a595670191605cec32999f0d28afd804f675be4
Mo Chen <sth4nth@gmail.com>
2016-01-13 10:34:11 +0800

    add mixLinRnd

--- chapter14/demo.m
+++ chapter14/demo.m
@@ -3,19 +3,10 @@ close all; clear
 d = 1;
 k = 2;
 n = 500;
-W = randn(d+1,k);
-
-[x, label] = kmeansRnd(d, k, n);
-X = [x; ones(1,n)];
-y = zeros(1,n);
-for j = 1:k
-    idx = (label == j);
-    y(idx) = W(:,j)'*X(:,idx);
-end
-
-plot(x,y,'.');
-[model, label,llh] = mixLinReg(X, y, k);
-plotClass([x;y],label);
+[X,y] = mixLinRnd(d,k,n);
+plot(X,y,'.');
+[model,label,llh] = mixLinReg(X, y, k);
+plotClass([X;y],label);
 figure
 plot(llh);
 
