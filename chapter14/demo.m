fce48f17d4a39e86df9ef3fecb2fb2903b7cf0e3
sth4nth <sth4nth@gmail.com>
2016-01-15 15:51:29 +0800

    update todo

--- chapter14/demo.m
+++ chapter14/demo.m
@@ -1,13 +1,23 @@
 % TODO: demo for mixLogitReg
 %% Demo for mixture of linear regression
+% close all; clear
+% d = 1;
+% k = 2;
+% n = 500;
+% [X,y] = mixLinRnd(d,k,n);
+% plot(X,y,'.');
+% [label,model,llh] = mixLinReg(X, y, k);
+% plotClass([X;y],label);
+% figure
+% plot(llh);
+
+%% Demo for adaboost
 close all; clear
-d = 1;
+d = 2;
 k = 2;
 n = 500;
-[X,y] = mixLinRnd(d,k,n);
-plot(X,y,'.');
-[label,model,llh] = mixLinReg(X, y, k);
-plotClass([X;y],label);
-figure
-plot(llh);
+[X,t] = kmeansRnd(d,k,n);
+plotClass(X,t);
+t = t-1;
 
+model = adaboost(X,t);
