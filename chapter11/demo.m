498dc35ad1f8d6bcfcfcf1b37972739a50b9898f
sth4nth <sth4nth@gmail.com>
2016-01-22 14:40:45 +0800

    added sequential update Gaussian demo

--- chapter11/demo.m
+++ chapter11/demo.m
@@ -1,23 +1,16 @@
+
+% demo for sequential update Gaussian 
+close all; clear;
 d = 2;
 n = 100;
 X = randn(d,n);
-x = rand(d,1);
+x = randn(d,1);
+
 mu = mean(X,2);
 Xo = bsxfun(@minus,X,mu);
 Sigma = Xo*Xo'/n;
+p1 = logGauss(x,mu,Sigma);
 
-XX = Xo*Xo'/n;
-XXX = X*X'/n-mu*mu';
-U = chol(XX);
-
-U_ = chol(Xo(:,2:end)*Xo(:,2:end)');
-UU = cholupdate(U,(X(:,1)-mu));
-
-
-
-
-p = logGauss(x,mu,Sigma);
-gauss = Gaussian(X(:,3:end));
-gauss = gauss.addSample(X(:,1));
-gauss = gauss.addSample(X(:,2));
+gauss = Gaussian(X(:,3:end)).addSample(X(:,1)).addSample(X(:,2));
 p2 = gauss.logPdf(x);
+abs(p1-p2)
