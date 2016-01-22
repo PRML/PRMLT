5d381487095640c9a05b620721fc8aeb66e8d240
Mo Chen <sth4nth@gmail.com>
2016-01-22 21:57:02 +0800

    undo

--- chapter11/Gaussian.m
+++ chapter11/Gaussian.m
@@ -57,19 +57,5 @@ classdef Gaussian
              c = d*log(2*pi)+2*sum(log(diag(U)));   % normalization constant
              y = -0.5*(c+q);
          end
-         
-         function y = logTPdf(obj, X)
-                 [R,p]= cholcov(sigma,0);
-                if p ~= 0
-                    error('ERROR: sigma is not SPD.');
-                end
-                X = bsxfun(@minus,X,mu);
-                Q = R'\X;
-                q = dot(Q,Q,1);  % quadratic term (M distance)
-                o = -log(1+q/v)*((v+d)/2);
-                c = gammaln((v+d)/2)-gammaln(v/2)-(d*log(v*pi)+2*sum(log(diag(R))))/2;
-                y = c+o;
-    
-         end
      end
 end
