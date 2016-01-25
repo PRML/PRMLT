20ed39b23f5b79a9f2801e3002fe5c5c05fc7d48
sth4nth <sth4nth@gmail.com>
2016-01-25 11:17:49 +0800

    rename

--- chapter11/GaussWishart.m
+++ chapter11/GaussWishart.m
@@ -1,61 +1,61 @@
-
-classdef Gaussian
-     properties
-         n_
+classdef GaussWishart
+        properties
+         kappa_
          mu_
+         nu_
          U_
      end
      
      methods
-         function obj = Gaussian(X)
-             n = size(X,2);
-             mu = mean(X,2);
-             U = chol(X*X');
+         function obj = GaussianWishart(kappa,mu,nu,W)
+             U = chol(W);
              
-             obj.n_ = n;
+             obj.kappa_ = kappa;
              obj.mu_ = mu;
+             obj.nu_ = nu;
              obj.U_ = U;
          end
         
          function obj = addSample(obj, x)
-             n = obj.n_;
+             kappa = obj.kappa_;
              mu = obj.mu_;
+             nu = obj.nu_;
              U = obj.U_;
              
-             n = n+1;
+             kappa = kappa+1;
              mu = mu+(x-mu)/n;
+             nu = nu+1;
              U = cholupdate(U,x,'+');
              
-             obj.n_ = n;
+             obj.kappa_ = kappa;
              obj.mu_ = mu;
+             obj.nu_ = nu;
              obj.U_ = U;
          end
          
          function obj = delSample(obj, x)
-             n = obj.n_;
+             kappa = obj.kappa_;
              mu = obj.mu_;
+             nu = obj.nu_;
              U = obj.U_;
 
-             n = n-1;
+             kappa = kappa-1;
              mu = mu-(x-mu)/n;
+             nu = nu-1;
              U = cholupdate(U,x,'-');
              
-             obj.n_ = n;
+             obj.kappa_ = kappa;
              obj.mu_ = mu;
+             obj.nu_ = nu;
              obj.U_ = U;
          end
          
-         function y = logPdf(obj,X)
-             n = obj.n_;
+         function y = logPredPdf(obj,X)
+             kappa = obj.kappa_;
              mu = obj.mu_;
+             nu = obj.nu_;
              U = obj.U_;
-             d = size(X,1);
-             
-             U = cholupdate(U/sqrt(n),mu,'-');       % Sigma=X*X'/n-mu*mu'
-             Q = U'\bsxfun(@minus,X,mu);
-             q = dot(Q,Q,1);  % quadratic term (M distance)
-             c = d*log(2*pi)+2*sum(log(diag(U)));   % normalization constant
-             y = -0.5*(c+q);
+                
          end
      end
 end
