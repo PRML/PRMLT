2ecbed4980e61d99228d02d26b7c408d3f376e8c
sth4nth <sth4nth@gmail.com>
2016-01-25 11:08:52 +0800

    temp

--- chapter11/Gaussian.m
+++ chapter11/Gaussian.m
@@ -51,25 +51,11 @@ classdef Gaussian
              U = obj.U_;
              d = size(X,1);
              
-             U = cholupdate(U/sqrt(n),mu,'-');       % Sigma=X*X'/n-mu*mu
+             U = cholupdate(U/sqrt(n),mu,'-');       % Sigma=X*X'/n-mu*mu'
              Q = U'\bsxfun(@minus,X,mu);
              q = dot(Q,Q,1);  % quadratic term (M distance)
              c = d*log(2*pi)+2*sum(log(diag(U)));   % normalization constant
              y = -0.5*(c+q);
-         end
-         
-         function y = logTPdf(obj, X)
-                 [R,p]= cholcov(sigma,0);
-                if p ~= 0
-                    error('ERROR: sigma is not SPD.');
-                end
-                X = bsxfun(@minus,X,mu);
-                Q = R'\X;
-                q = dot(Q,Q,1);  % quadratic term (M distance)
-                o = -log(1+q/v)*((v+d)/2);
-                c = gammaln((v+d)/2)-gammaln(v/2)-(d*log(v*pi)+2*sum(log(diag(R))))/2;
-                y = c+o;
-    
          end
      end
 end
