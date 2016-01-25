c0db46abd3852c41b06a58c5ed08852df48a8428
Mo Chen <sth4nth@gmail.com>
2016-01-25 22:38:27 +0800

    Gibbs not finished

--- chapter11/mixGaussGb.m
+++ chapter11/mixGaussGb.m
@@ -1,31 +1,57 @@
 function [label, model, llh] = mixGaussGb(X, prior)
 % Collapsed Gibbs sampling for (infinite) Gaussian mixture model (a.k.a.
 % DPGM)
-n = size(X,2);
-
-Theta = prior.theta;
-alpha = prior.alpha;
-Z = ones(1,n);
-maxIter = 1000;
-for iter = 1:maxIter
-    for i = randperm(n)
-        rest = true(1,n);
-        rest(i) = false;
-        x = X(:,i);
-        z = Z(:,i);
-        
-        Theta{z} = delSample(Theta{z},x);
-        logFx = cellfun(x,Theta.Pred);
-        logNk = log(sum(Z(:,rest),2));
-        logNk(1) = log(alpha);
-        logR = logFx+logNk;
-        p = exp(logR-logsumexp(logR));
-        z = discreteRnd(p);
-        Z(:,i) = z;
-        Theta{z} = addSample(Theta{z},x);
-        index = any(Z,2);
-        Z = [zeros(1,n);Z(index)];
-        Theta = {prior.theta,Theta{index}};                  % remove empty
-    end
+[d,n] = size(X);
+% parmaters of Gaussian-Wishart prior
+if nargin == 1
+    kappa0 = 1;
+    m0 = zeros(d,1);
+    nu0 = d;
+    S0 = eye(d);
+    alpha0 = 1;
+else
+    kappa0 = prior.kappa;
+    m0 = prior.m;
+    nu0 = prior.nu;
+    S0 = prior.S;
+    alpha0 = prior.alpha;
 end
 
+[Theta,Z] = init(X,GaussWishart(kappa0,m0,nu0,S0));
+maxIter = 50;
+for iter = 1:maxIter
+    for i = randperm(n)
+        x = X(:,i);
+        z = Z(:,i);
+        try
+        Theta{z} = Theta{z}.delSample(x);
+        catch
+            error('error!\n');
+        end
+        logPk = cellfun(@(theta) theta.logPredPdf(x),Theta)';
+        logNk = log(sum(Z(:,~id(i,n)),2));
+        logNk(1) = log(alpha0);
+        logR = logPk+logNk;
+        p = exp(logR-logsumexp(logR));
+        z = id(discreteRnd(p),numel(p));
+        Z(:,i) = z;
+        Theta{z} = Theta{z}.addSample(x);
+        ne = any(Z,2);   % non-empty
+        Z = [false(1,n);Z(ne,:)];
+        gw = GaussWishart(kappa0,m0,nu0,S0);
+        Theta = {gw,Theta{ne}};                  % remove empty
+    end
+end
+model = Theta{2:end};
+label = max(Z(2:end,:),[],1);
+
+function [Theta,Z] = init(X, theta)
+n = size(X,2);
+for i = randperm(n)
+    x = X(:,i);
+end
+
+
+function indicator = id(i, n)
+indicator = false(n,1);
+indicator(i) = true;
