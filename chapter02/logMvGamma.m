b7ac67bfef8df7fcb2b71676b34852e96a2cfee1
Mo Chen <sth4nth@gmail.com>
2016-01-27 21:00:03 +0800

    modified comment

--- chapter02/logMvGamma.m
+++ chapter02/logMvGamma.m
@@ -1,7 +1,7 @@
 function y = logMvGamma(x,d)
 % Compute logarithm multivariate Gamma function.
-% Gamma_p(x) = pi^(p(p-1)/4) prod_(j=1)^p Gamma(x+(1-j)/2)
-% log Gamma_p(x) = p(p-1)/4 log pi + sum_(j=1)^p log Gamma(x+(1-j)/2)
+% Gamma_p(x) = pi^(d(d-1)/4) \prod_(j=1)^d Gamma(x+(1-j)/2)
+% log(Gamma_p(x)) = d(d-1)/4 log(pi) + \sum_(j=1)^d log(Gamma(x+(1-j)/2))
 % Written by Michael Chen (sth4nth@gmail.com).
 s = size(x);
 x = reshape(x,1,prod(s));
