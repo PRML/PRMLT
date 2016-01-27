802c311ddb256b26d215f416d269dd7973aefe67
sth4nth <sth4nth@gmail.com>
2016-01-27 11:32:21 +0800

    added comment

--- chapter13/HMM/hmmFilter_.m
+++ chapter13/HMM/hmmFilter_.m
@@ -9,6 +9,6 @@ energy = zeros(1,T);
 alpha = zeros(K,T);
 [alpha(:,1),energy(1)] = normalize(s.*M(:,1),1);
 for t = 2:T
-    [alpha(:,t),energy(t)] = normalize((At*alpha(:,t-1)).*M(:,t),1);
+    [alpha(:,t),energy(t)] = normalize((At*alpha(:,t-1)).*M(:,t),1);    % 13.59
 end
 energy = sum(log(energy(energy>0)));
--- chapter13/HMM/hmmSmoother_.m
+++ chapter13/HMM/hmmSmoother_.m
@@ -7,11 +7,11 @@ c = zeros(1,T); % normalization constant
 alpha = zeros(K,T);
 [alpha(:,1),c(1)] = normalize(s.*M(:,1),1);
 for t = 2:T
-    [alpha(:,t),c(t)] = normalize((At*alpha(:,t-1)).*M(:,t),1);
+    [alpha(:,t),c(t)] = normalize((At*alpha(:,t-1)).*M(:,t),1);  % 13.59
 end
 beta = ones(K,T);
 for t = T-1:-1:1
-    beta(:,t) = A*(beta(:,t+1).*M(:,t+1))/c(t+1);
+    beta(:,t) = A*(beta(:,t+1).*M(:,t+1))/c(t+1);   % 13.62
 end
 gamma = alpha.*beta;
 
