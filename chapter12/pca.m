0034ee7901a01c480eb734d62a2908716fb684d4
Mo Chen <sth4nth@gmail.com>
2016-01-28 22:17:46 +0800

    cleaned pca.m

--- chapter12/pca.m
+++ chapter12/pca.m
@@ -1,16 +1,20 @@
-function model = pca(X, p)
+function [U, L, mu, err] = pca(X, m)
 % Principal component analysis
+% Input:
 %   X: dxn data matrix 
-%   p: target dimension
+%   m: target dimension
+% Output:
+%   U: Projection matrix
+%   L: Eigen values
+%   mu: mean
+%   err: recontruction error
 % Written by Mo Chen (sth4nth@gmail.com).
-xbar = mean(X,2);
-Xo = bsxfun(@minus,X,xbar);
+mu = mean(X,2);
+Xo = bsxfun(@minus,X,mu);
 S = Xo*Xo'/size(X,2);                   % 12.3
 [U,L] = eig(S);                         % 12.5
 [L,idx] = sort(diag(L),'descend');      
-U = U(:,idx(1:p));
-L = L(1:p);
+err = sum(L)-sum(L(1:m));
+U = U(:,idx(1:m));
+L = L(1:m);
 
-model.xbar = xbar;
-model.U = U;
-model.L = L;
