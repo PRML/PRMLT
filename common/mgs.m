83226955278fc5f0d8953a2277ff0b3514a93286
Mo Chen <sth4nth@gmail.com>
2016-01-28 22:17:06 +0800

    moved modified Gram-Schmidt

--- common/mgs.m
+++ common/mgs.m
@@ -1,5 +1,5 @@
-function X = gso(X)
-% Gram-Schmidt orthogonalization
+function X = mgs(X)
+% Modified Gram-Schmidt orthogonalization
 for i = 1:size(X,2)
     v = X(:,i)-X(:,1:i-1)*(X(:,1:i-1)'*X(:,i));
     X(:,i) = v/sqrt(dot(v,v));
