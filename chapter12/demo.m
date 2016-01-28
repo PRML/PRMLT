4f6701d1dfab4b15535cb08a4d0056ef22ee99a3
Mo Chen <sth4nth@gmail.com>
2016-01-28 23:18:01 +0800

    finished chapter12 (almost)

--- chapter12/demo.m
+++ chapter12/demo.m
@@ -11,28 +11,34 @@ n = 1000;
 
 X = ppcaRnd(m,d,n);
 plotClass(X);
-%% PCA , EM PCA and Constraint EM PCA produce the same result in the sense of reconstruction error
-% classical PCA
-[U,L,mu,err1] = pca(X,m);
-Y = U'*bsxfun(@minus,X,mu);   % projection
-Z1 = bsxfun(@times,Y,1./sqrt(L));  % whiten
-figure;
-plotClass(Y);
-figure;
-plotClass(Z1);
-err1
-% EM PCA
-[W2,Z2,mu,err2] = pcaEm(X,m);
-figure;
-plotClass(Z1);
-err2
-% Contrained EM PCA
-[W3,Z3,mu,err3] = pcaEmC(X,m);
-figure;
-plotClass(Z1);
-err3
-%% EM probabilistic PCA
-[W,mu,beta,llh] = ppcaEm(X,m);
+% %% PCA , EM PCA and Constraint EM PCA produce the same result in the sense of reconstruction error
+% % classical PCA
+% [U,L,mu,err1] = pca(X,m);
+% Y = U'*bsxfun(@minus,X,mu);   % projection
+% Z1 = bsxfun(@times,Y,1./sqrt(L));  % whiten
+% figure;
+% plotClass(Y);
 % figure;
 % plotClass(Z1);
-plot(llh)
+% err1
+% % EM PCA
+% [W2,Z2,mu,err2] = pcaEm(X,m);
+% figure;
+% plotClass(Z1);
+% err2
+% % Contrained EM PCA
+% [W3,Z3,mu,err3] = pcaEmC(X,m);
+% figure;
+% plotClass(Z1);
+% err3
+% %% EM probabilistic PCA
+% [W,mu,beta,llh] = ppcaEm(X,m);
+% plot(llh)
+
+%% Variational Bayesian probabilistic PCA
+[model, energy] = ppcaVb(X);
+plot(energy);
+
+%% factor analysis
+[W, mu, psi, llh] = fa(X, m);
+plot(llh);
