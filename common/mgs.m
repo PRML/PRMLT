12ab9de192401c6dca15062b6cf8405cce9c4018
Mo Chen <sth4nth@gmail.com>
2016-01-30 13:56:06 +0800

    added modified Gram-Schmidt orthogonalization algorithm

--- common/mgs.m
+++ common/mgs.m
@@ -1,6 +1,14 @@
-function X = mgs(X)
-% Modified Gram-Schmidt orthogonalization
-for i = 1:size(X,2)
-    v = X(:,i)-X(:,1:i-1)*(X(:,1:i-1)'*X(:,i));
-    X(:,i) = v/sqrt(dot(v,v));
+function [Q,R] = mgs(X)
+% Modified Gram-Schmidt orthogonalization (numerical stable version
+% Gram-Schmidt algorithm) which produces the same result as [Q,R]=qr(X,0)
+[d,n] = size(X);
+m = min(d,n);
+R = zeros(m,n);
+Q = zeros(d,m);
+for i = 1:m
+    R(1:i-1,i) = Q(:,1:i-1)'*X(:,i);
+    Q(:,i) = X(:,i)-Q(:,1:i-1)*R(1:i-1,i);
+    R(i,i) = norm(Q(:,i));
+    Q(:,i) = Q(:,i)/R(i,i);
 end
+R(:,m+1:n) = Q'*X(:,m+1:n);
