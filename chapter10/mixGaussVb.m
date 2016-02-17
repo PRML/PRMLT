3949283d5285a1e6c51d25bdbee2f8dda0ed2b4a
sth4nth <sth4nth@gmail.com>
2016-02-17 09:41:42 +0800

    vbgm not done

--- chapter10/mixGaussVb.m
+++ chapter10/mixGaussVb.m
@@ -13,18 +13,22 @@ if nargin < 3
     prior.v = d+1;
     prior.M = eye(d);   % M = inv(W)
 end
-tol = 1e-8;
-maxiter = 500;
+tol = 1e-20;
+maxiter = 5000;
 L = -inf(1,maxiter);
+lb = -inf(1,maxiter);
 model = init(X,m);
+model.nk = sum(model.R,1);
 for iter = 2:maxiter
-    model = q_pi(model,prior);                    % q(pi)
-    model = q_mu_Lambda(X, model, prior);          % q(mu,Lambda) 
-    model = q_z(X, model);                     % q(z)
-    L(iter) = bound(X,model,prior)/n;
+    [model, KLDir] = q_pi(model,prior);                    % q(pi)
+    [model, KLGW] = q_mu_Lambda(X, model, prior);          % q(mu,Lambda) 
+    [model, KLMul] = q_z(X, model);                     % q(z)
+    lb(iter) = KLDir+KLGW+KLMul;
+    L(iter) = bound(X,model,prior);
     if abs(L(iter)-L(iter-1)) < tol*abs(L(iter)); break; end
 end
 L = L(2:iter);
+lb = lb(2:iter);
 label = zeros(1,n);
 [~,label(:)] = max(model.R,[],2);
 [~,~,label(:)] = unique(label);
@@ -48,27 +52,37 @@ else
 end
 
 % Done
-function model = q_pi(model, prior)
+function [model, KL] = q_pi(model, prior)
 alpha0 = prior.alpha;
 R = model.R;
+nk = model.nk;
 
-nk = sum(R,1); % 10.51
+k = size(R,2);
+
 alpha = alpha0+nk; % 10.58
 ElogPi = psi(0,alpha)-psi(0,sum(alpha)); % 10.66
+
+% lower bound
+logCalpha0 = gammaln(k*alpha0)-k*gammaln(alpha0);
+Eppi = logCalpha0+(alpha0-1)*sum(ElogPi);
+logCalpha = gammaln(sum(alpha))-sum(gammaln(alpha));
+Eqpi = dot(alpha-1,ElogPi)+logCalpha;
+KL = Eppi-Eqpi;
 
 model.alpha = alpha;    
 model.ElogPi = ElogPi;
 
 % Done
-function model = q_mu_Lambda(X, model, prior)
+function [model, KL] = q_mu_Lambda(X, model, prior)
 kappa0 = prior.kappa;
 m0 = prior.m;
 v0 = prior.v;
 M0 = prior.M;
 R = model.R;
 alpha = model.alpha;
+nk = model.nk;
 
-nk = sum(R,1); % 10.51
+
 kappa = kappa0+nk; % 10.60
 m = bsxfun(@plus,X*R,kappa0*m0);
 m = bsxfun(@times,m,1./kappa); % 10.61
@@ -84,14 +98,56 @@ for i = 1:k
     M(:,:,i) = M0+Xm*Xm'+kappa0*(m0m*m0m');     % equivalent to 10.62
 end
 
+
+
+
+U0 = chol(M0);
+sqrtR = sqrt(R);
+xbar = bsxfun(@times,X*R,1./nk); % 10.52
+
+logW = zeros(1,k);
+trSW = zeros(1,k);
+trM0W = zeros(1,k);
+xbarmWxbarm = zeros(1,k);
+mm0Wmm0 = zeros(1,k);
+for i = 1:k
+    U = chol(M(:,:,i));
+    logW(i) = -2*sum(log(diag(U)));      
+    
+    Xs = bsxfun(@times,bsxfun(@minus,X,xbar(:,i)),sqrtR(:,i)');
+    V = chol(Xs*Xs'/nk(i));
+    Q = V/U;
+    trSW(i) = dot(Q(:),Q(:));  % equivalent to tr(SW)=trace(S/M)
+    Q = U0/U;
+    trM0W(i) = dot(Q(:),Q(:));
+
+    q = U'\(xbar(:,i)-m(:,i));
+    xbarmWxbarm(i) = dot(q,q);
+    q = U'\(m(:,i)-m0);
+    mm0Wmm0(i) = dot(q,q);
+end
+
+ElogLambda = sum(psi(0,bsxfun(@minus,v+1,(1:d)')/2),1)+d*log(2)+logW; % 10.65
+Epmu = sum(d*log(kappa0/(2*pi))+ElogLambda-d*kappa0./kappa-kappa0*(v.*mm0Wmm0))/2;
+logB0 = v0*sum(log(diag(U0)))-0.5*v0*d*log(2)-logMvGamma(0.5*v0,d);
+EpLambda = k*logB0+0.5*(v0-d-1)*sum(ElogLambda)-0.5*dot(v,trM0W);
+
+Eqmu = 0.5*sum(ElogLambda+d*log(kappa/(2*pi)))-0.5*d*k;
+logB =  -v.*(logW+d*log(2))/2-logMvGamma(0.5*v,d);
+EqLambda = 0.5*sum((v-d-1).*ElogLambda-v*d)+sum(logB);
+
+EpX = 0.5*dot(nk,ElogLambda-d./kappa-v.*trSW-v.*xbarmWxbarm-d*log(2*pi));
+
+
+KL = Epmu-Eqmu+EpLambda-EqLambda+EpX;
 model.alpha = alpha;
 model.kappa = kappa;
 model.m = m;
 model.v = v;
 model.M = M; % Whishart: M = inv(W)
 
-% not Done
-function model = q_z(X, model)
+% Done
+function [model, KL] = q_z(X, model)
 ElogPi = model.ElogPi;
 kappa = model.kappa;   % Gaussian
 m = model.m;         % Gasusian
@@ -113,9 +169,15 @@ ElogLambda = sum(psi(0,bsxfun(@minus,v+1,(1:d)')/2),1)+d*log(2)+logW; % 10.65
 logRho = -0.5*bsxfun(@minus,EQ,2*ElogPi+ElogLambda-d*log(2*pi)); % 10.46
 logR = bsxfun(@minus,logRho,logsumexp(logRho,2)); % 10.49
 R = exp(logR);
+nk = sum(R,1); % 10.51
+% lower bound
+Epz = dot(nk,ElogPi);
+Eqz = dot(R(:),logR(:));
+KL = Epz-Eqz;
 
 model.logR = logR;
 model.R = R;
+model.nk = nk;
 % Done
 function L = bound(X, model, prior)
 alpha0 = prior.alpha;
@@ -131,10 +193,9 @@ v = model.v;         % Whishart
 M = model.M;         % Whishart: inv(W) = V'*V
 R = model.R;
 logR = model.logR;
-
+nk = model.nk;
 
 [d,k] = size(m);
-nk = sum(R,1); % 10.51
 
 Elogpi = psi(0,alpha)-psi(0,sum(alpha));
 
