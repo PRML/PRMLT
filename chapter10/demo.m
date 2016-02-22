6ab1c9d7679b966c3938d31922dc74c8d4215bd1
sth4nth <sth4nth@gmail.com>
2016-02-22 17:44:40 +0800

    added mxGaussVbPred

--- chapter10/demo.m
+++ chapter10/demo.m
@@ -1,36 +1,42 @@
 % demos for ch10
 % chapter10/12: prediction functions for VB
-%% regression
-clear; close all;
-
-d = 100;
-beta = 1e-1;
-X = rand(1,d);
-w = randn;
-b = randn;
-t = w'*X+b+beta*randn(1,d);
-x = linspace(min(X),max(X),d);   % test data
-
-[model,llh] = linRegVb(X,t);
-% [model,llh] = rvmRegVb(X,t);
-plot(llh);
-[y, sigma] = linRegPred(model,x,t);
-figure
-plotCurveBar(x,y,sigma);
-hold on;
-plot(X,t,'o');
-hold off
+%% Variational Bayesian for linear\RVM regression
+% clear; close all;
+% 
+% d = 100;
+% beta = 1e-1;
+% X = rand(1,d);
+% w = randn;
+% b = randn;
+% t = w'*X+b+beta*randn(1,d);
+% x = linspace(min(X),max(X),d);   % test data
+% 
+% [model,llh] = linRegVb(X,t);
+% % [model,llh] = rvmRegVb(X,t);
+% plot(llh);
+% [y, sigma] = linRegPred(model,x,t);
+% figure
+% plotCurveBar(x,y,sigma);
+% hold on;
+% plot(X,t,'o');
+% hold off
 %% Variational Bayesian for Gaussian Mixture Model
-% close all; clear;
-% d = 2;
-% k = 3;
-% n = 2000;
-% [X,label] = mixGaussRnd(d,k,n);
-% plotClass(X,label);
-% [y, model, L] = mixGaussVb(X,10);
-% figure;
-% plotClass(X,y);
-% figure;
-% plot(L)
-
+close all; clear;
+d = 2;
+k = 3;
+n = 2000;
+[X,z] = mixGaussRnd(d,k,n);
+plotClass(X,z);
+Xt = X(:,n/2+1:end);
+X = X(:,1:n/2);
+% VB fitting
+[y, model, L] = mixGaussVb(X,10);
+figure;
+plotClass(X,y);
+figure;
+plot(L)
+% Predict testing data
+[yt, R] = mixGaussVbPred(model,Xt);
+figure;
+plotClass(Xt,yt);
 
