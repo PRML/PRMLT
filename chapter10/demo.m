bb73942ff7a16bdc216fee855e7225d4fba23880
sth4nth <sth4nth@gmail.com>
2016-02-22 17:13:41 +0800

    refactorized plotCurveBar

--- chapter10/demo.m
+++ chapter10/demo.m
@@ -1,39 +1,36 @@
 % demos for ch10
 % chapter10/12: prediction functions for VB
 %% regression
-% clear; close all;
-% 
-% d = 100;
-% beta = 1e-1;
-% X = rand(1,d);
-% w = randn;
-% b = randn;
-% t = w'*X+b+beta*randn(1,d);
-% x = linspace(min(X)-1,max(X)+1,d);   % test data
-% 
-% [model,llh] = linRegVb(X,t);
-% % [model,llh] = rvmRegVb(X,t);
-% figure
-% plot(llh);
-% [y, sigma] = linRegPred(model,x);
-% figure;
-% hold on;
-% plotBand(x,y,2*sigma);
-% plot(X,t,'o');
-% plot(x,y,'r-');
-% hold off
+clear; close all;
 
+d = 100;
+beta = 1e-1;
+X = rand(1,d);
+w = randn;
+b = randn;
+t = w'*X+b+beta*randn(1,d);
+x = linspace(min(X)-1,max(X)+1,d);   % test data
+
+[model,llh] = linRegVb(X,t);
+% [model,llh] = rvmRegVb(X,t);
+plot(llh);
+[y, sigma] = linRegPred(model,x,t);
+figure
+plotCurveBar(x,y,sigma);
+hold on;
+plot(X,t,'o');
+hold off
 %% Variational Bayesian for Gaussian Mixture Model
-close all; clear;
-d = 2;
-k = 3;
-n = 2000;
-[X,label] = mixGaussRnd(d,k,n);
-plotClass(X,label);
-[y, model, L] = mixGaussVb(X,10);
-figure;
-plotClass(X,y);
-figure;
-plot(L)
+% close all; clear;
+% d = 2;
+% k = 3;
+% n = 2000;
+% [X,label] = mixGaussRnd(d,k,n);
+% plotClass(X,label);
+% [y, model, L] = mixGaussVb(X,10);
+% figure;
+% plotClass(X,y);
+% figure;
+% plot(L)
 
 
--- chapter13/LDS/demo.m
+++ chapter13/LDS/demo.m
@@ -2,6 +2,8 @@
 % 1) test against matlab implementation of kalman filter
 % 2) simplify ldsEm with less parameters (G=diag(g), S=I) 
 
+% demos for LDS in ch13
+
 clear; close all;
 d = 3;
 k = 2;
