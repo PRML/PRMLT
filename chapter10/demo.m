4f217ee51863359144171e9d0a83ffa7c1a1595f
sth4nth <sth4nth@gmail.com>
2016-02-22 17:22:56 +0800

    modified linRegPred to work when U is not provided

--- chapter10/demo.m
+++ chapter10/demo.m
@@ -9,7 +9,7 @@ X = rand(1,d);
 w = randn;
 b = randn;
 t = w'*X+b+beta*randn(1,d);
-x = linspace(min(X)-1,max(X)+1,d);   % test data
+x = linspace(min(X),max(X),d);   % test data
 
 [model,llh] = linRegVb(X,t);
 % [model,llh] = rvmRegVb(X,t);
