d653d6cd33ff706bac3aff1648c9f6dd80b1b993
sth4nth <sth4nth@gmail.com>
2016-02-22 18:14:00 +0800

    added mlp stump

---
+++ chapter05/mlp.m
@@ -0,0 +1,10 @@
+function [ model, err ] = mlp(X, h)
+% Multilayer perceptron neural network for regression
+% Input:   
+%   X: d x n data matrix
+%   h: l vector with that each h(i) is number of hidden nodes in layer l
+
+
+
+
+
