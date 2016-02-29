e793d373633b9ea03757ba9bccbbc6fadf5155f2
Mo Chen <sth4nth@gmail.com>
2016-03-01 00:25:22 +0800

    renamed mse

--- chapter12/pca.m
+++ chapter12/pca.m
@@ -1,4 +1,4 @@
-function [U, L, mu, err] = pca(X, m)
+function [U, L, mu, mse] = pca(X, m)
 % Principal component analysis
 % Input:
 %   X: d x n data matrix 
@@ -7,14 +7,15 @@ function [U, L, mu, err] = pca(X, m)
 %   U: d x m Projection matrix
 %   L: m x 1 Eigen values
 %   mu: d x 1 mean
-%   err: recontruction error
+%   mse: mean square error
 % Written by Mo Chen (sth4nth@gmail.com).
+n = size(X,2);
 mu = mean(X,2);
 Xo = bsxfun(@minus,X,mu);
-S = Xo*Xo'/size(X,2);                   % 12.3
+S = Xo*Xo'/n;                   % 12.3
 [U,L] = eig(S);                         % 12.5
 [L,idx] = sort(diag(L),'descend');      
-err = sum(L)-sum(L(1:m));
+mse = sum(L)-sum(L(1:m));
 U = U(:,idx(1:m));
 L = L(1:m);
 
