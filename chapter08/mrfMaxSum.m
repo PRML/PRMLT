d4d8cb063bf1dc087b0302f4766257dbc385bab9
sth4nth <sth4nth@gmail.com>
2016-03-01 13:44:00 +0800

    updated bp stumps

--- chapter08/mrfMaxSum.m
+++ chapter08/mrfMaxSum.m
@@ -1,6 +1,9 @@
-function [ output_args ] = mrfMaxSum( input_args )
-% Max-sum loopy belief propagation on Markov random field 
+function [z, llh] = mrfMaxSum(W)
+% Max-sum loopy belief propagation on Markov random field with discrete rvs
 % Input:
 %   W: n x n sparse weight matrix of a graph
+% Output:
+%   z: 1 x n vector of predicted label
+%   llh: loglikelihood
 % Written by Mo Chen (sth4nth@gmail.com).
 
--- chapter08/mrfSumProd.m
+++ chapter08/mrfSumProd.m
@@ -1,8 +1,8 @@
-function model = mrfSumProd(W)
-% Sum-product loopy belief propagation on Markov random field 
+function [z, llh] = mrfSumProd(W)
+% Sum-product loopy belief propagation on Markov random field with discrete rvs
 % Input:
 %   W: n x n sparse weight matrix of a graph
+% Output:
+%   z: 1 x n vector of predicted label
+%   llh: loglikelihood
 % Written by Mo Chen (sth4nth@gmail.com).
-
-
-
