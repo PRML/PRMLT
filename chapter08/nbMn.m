ed8fac2448b6551513d3f1bb2ea5bb0df43ee6f0
Mo Chen <sth4nth@gmail.com>
2016-03-06 16:33:06 +0800

    added naive bayes multinomial stub

---
+++ chapter08/nbMn.m
@@ -0,0 +1,7 @@
+function [ output_args ] = nbMn( input_args )
+%NBMN Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
---
+++ chapter08/nbMnPred.m
@@ -0,0 +1,7 @@
+function [ output_args ] = nbMnPred( input_args )
+%NBMNPRED Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
