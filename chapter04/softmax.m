03bdc614a3438b17985ae508965496e4a0ffe819
Mo Chen <sth4nth@gmail.com>
2017-02-14 16:59:36 +0800

    tweak logsumexp a little. nothing serious

--- chapter04/softmax.m
+++ chapter04/softmax.m
@@ -2,7 +2,7 @@ function s = softmax(x, dim)
 % Softmax function
 %   By default dim = 1 (columns).
 % Written by Mo Chen (sth4nth@gmail.com).
-if nargin == 1, 
+if nargin == 1
     % Determine which dimension sum will use
     dim = find(size(x)~=1,1);
     if isempty(dim), dim = 1; end
--- common/logsumexp.m
+++ common/logsumexp.m
@@ -3,15 +3,10 @@ function s = logsumexp(X, dim)
 %   By default dim = 1 (columns).
 % Written by Mo Chen (sth4nth@gmail.com).
 if nargin == 1
-    % Determine which dimension sum will use
     dim = find(size(X)~=1,1);
     if isempty(dim), dim = 1; end
 end
-
-% subtract the largest in each dim
-y = max(X,[],dim);
-s = y+log(sum(exp(bsxfun(@minus,X,y)),dim));   % TODO: use log1p
-i = isinf(y);
-if any(i(:))
-    s(i) = y(i);
-end
+a = max(X,[],dim);
+s = a+log(sum(exp(X-a),dim));   % TODO: use log1p
+i = isinf(a);
+s(i) = a(i);
