03857d11afa607f643c7e6f516164c1e4ca8d658
Mo Chen <sth4nth@gmail.com>
2017-02-14 18:33:09 +0800

    tweak softmax

--- chapter04/softmax.m
+++ chapter04/softmax.m
@@ -1,10 +1,10 @@
-function s = softmax(x, dim)
+function [Y,s] = softmax(X, dim)
 % Softmax function
 %   By default dim = 1 (columns).
 % Written by Mo Chen (sth4nth@gmail.com).
 if nargin == 1
-    % Determine which dimension sum will use
-    dim = find(size(x)~=1,1);
+    dim = find(size(X)~=1,1);
     if isempty(dim), dim = 1; end
 end
-s = exp(bsxfun(@minus,x,logsumexp(x,dim)));
+s = logsumexp(X,dim);
+Y = exp(X-s);
