e0593a1f6ba897e5dc38abb41ded6b62c407545c
Mo Chen <sth4nth@gmail.com>
2017-03-11 15:09:24 +0800

    tweak kmedoids

--- chapter09/kmedoids.m
+++ chapter09/kmedoids.m
@@ -1,12 +1,12 @@
-function [label, energy, index] = kmedoids(X, init)
+function [label, index, energy] = kmedoids0(X, init)
 % Perform k-medoids clustering.
 % Input:
 %   X: d x n data matrix
 %   init: k number of clusters or label (1 x n vector)
 % Output:
 %   label: 1 x n cluster label
-%   energy: optimization target value
 %   index: index of medoids
+%   energy: optimization target value
 % Written by Mo Chen (sth4nth@gmail.com).
 [d,n] = size(X);
 if numel(init)==1
@@ -15,15 +15,14 @@ if numel(init)==1
 elseif numel(init)==n
     label = init;
 end
-X = bsxfun(@minus,X,mean(X,2));             % reduce chance of numerical problems
+X = X-mean(X,2);             % reduce chance of numerical problems
 v = dot(X,X,1);
-D = bsxfun(@plus,v,v')-2*(X'*X);            % Euclidean distance matrix
+D = v+v'-2*(X'*X);            % Euclidean distance matrix
 D(sub2ind([d,d],1:d,1:d)) = 0;              % reduce chance of numerical problems
-last = 0;
+last = zeros(1,n);
 while any(label ~= last)
-    [u,~,label(:)] = unique(label);   % remove empty clusters
-    [~, index] = min(D*sparse(1:n,label,1,n,numel(u),n),[],1);  % find k medoids
-    last = label;
+    [~,~,last(:)] = unique(label);   % remove empty clusters
+    [~, index] = min(D*sparse(1:n,last,1),[],1);  % find k medoids
     [val, label] = min(D(index,:),[],1);                % assign labels
 end
 energy = sum(val);
--- demo/ch09/kmedoids_demo.m
+++ demo/ch09/kmedoids_demo.m
@@ -3,7 +3,9 @@ d = 2;
 k = 3;
 n = 5000;
 [X,label] = kmeansRnd(d,k,n);
-y = kmedoids(X,k);
+init = ceil(k*rand(1,n));
+[y, idx, v] = kmedoids(X,init);
 plotClass(X,label);
 figure;
 plotClass(X,y);
+
