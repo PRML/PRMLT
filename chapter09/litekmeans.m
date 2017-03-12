3a9055dfbdb5e3ee78c4caea94da0deeacfcf041
Mo Chen <sth4nth@gmail.com>
2017-03-13 01:57:54 +0800

    tweak kmeans to the extreme

--- chapter09/litekmeans.m
+++ chapter09/litekmeans.m
@@ -1,17 +1,16 @@
-function [label, mu] = litekmeans(X, k)
-n = size(X,2);
-last = zeros(1,n);
-label = ceil(k*rand(1,n));
+function [label, mu] = litekmeans(X, label)
+idx = 1:size(X,2);
+last = idx;
 while any(label ~= last)
     [~,~,last(:)] = unique(label);            % remove empty clusters
-    mu = X*normalize(sparse(1:n,last,1),1);    % compute cluster centers 
+    mu = X*normalize(sparse(idx,last,1),1);    % compute cluster centers 
     [~,label] = min(dot(mu,mu,1)'/2-mu'*X,[],1); % assign sample labels
 end
 % Perform kmeans clustering.
 % Input:
 %   X: d x n data matrix
-%   k: number of clusters
+%   label: initial sample labels
 % Output:
-%   label: 1 x n cluster label
+%   label: 1 x n sample label
 %   mu: d x k center of clusters
 % Written by Mo Chen (sth4nth@gmail.com).
