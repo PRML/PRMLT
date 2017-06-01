92c822088cf56b934c8c2afa3b344f57923ab325
Mo Chen <sth4nth@gmail.com>
2017-06-02 04:57:20 +0800

    fix mlp

--- chapter05/mlp.m
+++ chapter05/mlp.m
@@ -11,11 +11,11 @@ function [model, mse] = mlp(X, T, h)
 eta = 1/size(X,2);
 h = [size(X,1);h(:);size(T,1)];
 L = numel(h);
-W = cell(L-1);
+W = cell(L-1,1);
 for l = 1:L-1
     W{l} = randn(h(l),h(l+1));
 end
-Z = cell(1,L);
+Z = cell(L,1);
 Z{1} = X;
 maxiter = 200;
 mse = zeros(1,maxiter);
