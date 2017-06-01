e1c19a5a13949b00e6a1ebaaad64d69fa673bd2b
Mo Chen <sth4nth@gmail.com>
2017-06-02 05:11:07 +0800

    refine mlpPred.m

--- chapter05/mlpPred.m
+++ chapter05/mlpPred.m
@@ -7,8 +7,7 @@ function Y = mlpPred(model, X)
 %   Y: p x n response matrix
 % Written by Mo Chen (sth4nth@gmail.com).
 W = model.W;
-L = length(W)+1;
 Y = X;
-for l = 2:L
-    Y = sigmoid(W{l-1}'*Y);
+for l = 1:length(W)
+    Y = sigmoid(W{l}'*Y);
 end
