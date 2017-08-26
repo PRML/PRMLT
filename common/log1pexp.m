159d5cd4ab2ae4ce873f14901fcd81706940e86d
Mo Chen <sth4nth@gmail.com>
2017-08-26 19:35:50 +0800

    update log1pexp log1mexp

--- common/log1pexp.m
+++ common/log1pexp.m
@@ -1,7 +1,8 @@
 function y = log1pexp(x)
 % Accurately compute y = log(1+exp(x))
-% reference: Accurately Computing log(1-exp(|a|)) Martin Machler
-seed = 33.3;
+% reference: Accurately Computing log(1-exp(-|a|)) Martin Machler
 y = x;
-idx = x<seed;
-y(idx) = log1p(exp(x(idx)));
+i = x > 18;
+j = i & (x <= 33.3);
+y(~i) = log1p(exp(x(~i)));
+y(j) = x(j)+exp(-x(j));
