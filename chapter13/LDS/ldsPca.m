fb04ac210add4ca4d958d58bcdde921c2bd84444
Mo Chen <sth4nth@gmail.com>
2018-11-29 03:38:27 +0800

    add empty ldsPca

---
+++ chapter13/LDS/ldsPca.m
@@ -0,0 +1,12 @@
+function [A, C, Z] = ldsPca(X, k, m)
+% Subspace method for learning linear dynamic system.
+% Input:
+%   X: d x n data matrix
+%   k: dimension of hidden variable
+%   m: 
+% Output:
+%   A: 
+%   C:
+%   Z:
+% reference: Bayesian Reasoning and Machine Learning (BRML) chapter 24.5.3 p.507
+% Written by Mo Chen (sth4nth@gmail.com).
